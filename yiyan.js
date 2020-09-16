3eff9cdf1d6ca87d09603827116b717554fe843f
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2020-09-16 21:14:44 +0800

    Create yiyan.js

---
+++ yiyan.js
@@ -0,0 +1,28 @@
+{
+  "rules" : [
+    {
+      "action" : "body",
+      "matchField" : "",
+      "field" : "",
+      "value" : "viptype\":\"4\"",
+      "matchValue" : "viptype\":\"1\"",
+      "destiontion" : "response",
+      "isRegex" : false
+    }
+  ],
+  "enabled" : false,
+  "isReadOnly" : false,
+  "name" : "一言",
+  "locations" : [
+    {
+      "method" : "",
+      "scheme" : "",
+      "enabled" : true,
+      "port" : 0,
+      "query" : "",
+      "host" : "115.28.168.103",
+      "path" : ""
+    }
+  ],
+  "description" : "微信公众号墨鱼手记，仅供学习交流使用"
+}
