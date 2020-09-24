2b0c14104dac941c3a96d0fdd451f170a0c78ead
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2020-09-24 22:12:42 +0800

    Create 西窗烛.js

---
+++ 西窗烛.js
@@ -0,0 +1,28 @@
+{
+  "rules" : [
+    {
+      "action" : "body",
+      "matchField" : "",
+      "field" : "",
+      "value" : "Membership\":true",
+      "matchValue" : "Membership\":false",
+      "destiontion" : "response",
+      "isRegex" : false
+    }
+  ],
+  "enabled" : false,
+  "isReadOnly" : false,
+  "name" : "西窗烛",
+  "locations" : [
+    {
+      "method" : "",
+      "scheme" : "",
+      "enabled" : true,
+      "port" : 0,
+      "query" : "",
+      "host" : "avoscloud.com",
+      "path" : ""
+    }
+  ],
+  "description" : "微信公众号墨鱼手记，仅供学习交流使用"
+}
